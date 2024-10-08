02a91a10b1b4a09026120e6016ff0db9be86ffb3
SharmelRayen <sharmelrayen@gmail.com>
2024-10-08 10:08:09 +0530

    First Commit

--- run.py
+++ run.py
@@ -19,7 +19,7 @@ def generate_certificates():
 
    for index, name in enumerate(list_of_names):
       certificate_template_image = cv2.imread("certificate-template.jpg")
-      cv2.putText(certificate_template_image, name.strip(), (815,1500), cv2.FONT_HERSHEY_SIMPLEX, 5, (0, 0, 250), 5, cv2.LINE_AA)
+      cv2.putText(certificate_template_image, name.strip(), (1058,1395), cv2.FONT_HERSHEY_COMPLEX, 3, (25,25,25), 3, cv2.LINE_AA)
       cv2.imwrite("generated-certificates/{}.jpg".format(name.strip()), certificate_template_image)
       print("Processing {} / {}".format(index + 1,len(list_of_names)))
       
